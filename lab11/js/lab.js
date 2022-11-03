b17781d3495fb72cd9c76728330b8b9dffb66f61
Sky Casey <theocasey11@gmail.com>
2022-11-03 09:43:55 -0700

    better lab11 buttons

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -8,16 +8,24 @@ $("#c_button").click(function () {
     // VV div id you would like to change
     //                              VV class you would like to toggle
     $("#challenges").toggleClass("special");
+    $("#c_button").toggleClass("special");
+
 });
 
 $("#p_button").click(function () { 
     $("#problems").toggleClass("special");
+    $("#p_button").toggleClass("special");
+
 });
 
 $("#r_button").click(function () { 
     $("#results").toggleClass("special");
+    $("#r_button").toggleClass("special");
+
 });
 
 $("#o_button").click(function () { 
     $("#output").toggleClass("special");
+    $("#o_button").toggleClass("special");
+
 });
