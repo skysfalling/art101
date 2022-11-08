0eeff053b6205e0c36c4ae0c164927dc13eef553
Sky Casey <theocasey11@gmail.com>
2022-11-07 19:18:25 -0800

    lab13

---
+++ lab13/js/lab.js
@@ -0,0 +1,32 @@
+/* Author: Sky Casey
+* Created: Nov 7
+* License: Public Domain
+*/
+
+// VV button  . click to start function
+$("#sort_button").click(function () { 
+    fizzbuzz();
+});
+
+
+function fizzbuzz(){
+
+    // create out string 
+    const out_string = "";
+
+    // loop through numbers 1 - 200
+
+        // if num is multiple of 3
+            // add "Fizz!" to outsring
+
+        // if num is multiple of 5
+            // add "Buzz!" to outstring
+
+        // if num is multiple of 7
+            // add "Boom!" to outstring
+
+        
+
+    $("#out_result").html(out_string);
+
+}
