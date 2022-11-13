93c991715869e51897153e2d357dc61168606aa3
Sky Casey <theocasey11@gmail.com>
2022-11-13 08:42:52 -0800

    lab13 update

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -15,13 +15,23 @@ $("#sort_button").click(function () {
 function sortingHat(input){
 
     // get the length of 'input' string
+    const length = input.length;
 
     // use modulus (% operator) to get the remainder with 4: mod = length % 4;
+    const mod = length % 4;
 
     /* create a conditional that will return 
     Gryffindor, Ravenclaw, Slytherin, and Hufflepuff 
     depending on whether the value of mod is 0, 1, 2, or 3 */
-    sortingHatResult = "dementor";
+    sortingHatResult = "";
+    if (length > 0){
+        if (mod == 0) { sortingHatResult = "Gryffindor"; }
+        else if (mod == 1) { sortingHatResult = "Ravenclaw"; }
+        else if (mod == 2) { sortingHatResult = "Slytherin"; }
+        else if (mod == 4) { sortingHatResult = "Hufflepuff"; }
+
+    }
+
 
 
     // CREATE A NEW ELEMENT, WITH nameSorted AS TEXT
--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -3,30 +3,50 @@
 * License: Public Domain
 */
 
+outputEl = document.getElementById("out_result");
+
 // VV button  . click to start function
-$("#sort_button").click(function () { 
-    fizzbuzz();
+$("#input_button").click(function () { 
+    var input = document.querySelector("input").valueAsNumber; // get input
+    fizzbuzz(input); // call function
 });
 
 
-function fizzbuzz(){
+function fizzbuzz(max){
 
     // create out string 
-    const out_string = "";
-
     // loop through numbers 1 - 200
-
+    for (i = 1; i < max; i++)
+    {
+        out_string = "";
         // if num is multiple of 3
+        if (i % 3 == 0)
+        {
             // add "Fizz!" to outsring
+            out_string += "Fizz!";
+        }
 
         // if num is multiple of 5
+        if (i % 5 == 0)
+        {
             // add "Buzz!" to outstring
+            out_string += "Buzz!";
+        }
 
         // if num is multiple of 7
+        if (i % 7 == 0){
             // add "Boom!" to outstring
+            out_string += "Boom!";
+        }
 
-        
+       outputToPage(i + ": " + out_string);
+    }
 
-    $("#out_result").html(out_string);
+}
 
+function outputToPage(str) {
+    var newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    newEl.classList.add("out_table")
+    outputEl.appendChild(newEl);
 }
